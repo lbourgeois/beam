296ee437a8a7e0373c13dae23c9f047f837debd0
BOURGEOIS Laurent <lbourgeois@talend.com>
2017-06-23 01:42:00 +0200

    POC for a pipeline with handled exceptions raised from DoFn

--- examples/java8/src/main/java/org/talend/beam/examples/ExceptionHandlerDoFn.java
+++ examples/java8/src/main/java/org/talend/beam/examples/ExceptionHandlerDoFn.java
@@ -7,77 +7,91 @@ import org.apache.beam.sdk.transforms.reflect.DoFnInvoker;
 import org.apache.beam.sdk.transforms.reflect.DoFnInvokers;
 import org.apache.beam.sdk.transforms.splittabledofn.RestrictionTracker;
 import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.util.UserCodeException;
+import org.apache.beam.sdk.values.TupleTag;
+
+import java.io.Serializable;
 
 /**
  * Created by lbourgeois on 21/06/17.
  */
-public class ExceptionHandlerDoFn<InputT, OutputT> extends DoFn<InputT, OutputT> {
+public class ExceptionHandlerDoFn<InputT extends Serializable, OutputT extends Serializable> extends DoFn<InputT, OutputT> {
 
-    private final DoFnInvoker fnInvoker;
-    private DoFn<InputT, OutputT> wrappedDoFn;
+    public final TupleTag<OutputT> mainOutput =
+            new TupleTag<OutputT>("mainOutput") {
+            };
+    public final TupleTag<InputT> errorOutput =
+            new TupleTag<InputT>("errorOutput") {
+            };
+    private transient DoFnInvoker fnInvoker;
+    private DoFn wrappedDoFn;
 
     public ExceptionHandlerDoFn(DoFn wrappedDoFn) {
 
         super();
-        wrappedDoFn = wrappedDoFn;
-        fnInvoker = DoFnInvokers.invokerFor(wrappedDoFn);
+        this.wrappedDoFn = wrappedDoFn;
     }
 
     @ProcessElement
     public void processElement(ProcessContext processContext) {
-        fnInvoker.invokeProcessElement(       new DoFnInvoker.ArgumentProvider<InputT, OutputT>() {
-            @Override
-            public DoFn<InputT, OutputT>.ProcessContext processContext(
-                    DoFn<InputT, OutputT> doFn) {
-                return processContext;
-            }
 
-            @Override
-            public RestrictionTracker<?> restrictionTracker() {
-                throw new UnsupportedOperationException("RestrictionTracker parameters are not supported.");
-            }
+        fnInvoker = DoFnInvokers.invokerFor(wrappedDoFn);
 
-            // Unsupported methods below.
 
-            @Override
-            public BoundedWindow window() {
-                throw new UnsupportedOperationException(
-                        "Access to window of the element not supported in Splittable DoFn");
-            }
+        try {
+            fnInvoker.invokeProcessElement(new DoFnInvoker.ArgumentProvider<InputT, OutputT>() {
+                @Override
+                public DoFn<InputT, OutputT>.ProcessContext processContext(
+                        DoFn<InputT, OutputT> doFn) {
+                    return processContext;
+                }
 
-            @Override
-            public StartBundleContext startBundleContext(DoFn<InputT, OutputT> doFn) {
-                throw new IllegalStateException(
-                        "Should not access startBundleContext() from @"
-                                + DoFn.ProcessElement.class.getSimpleName());
-            }
+                @Override
+                public RestrictionTracker<?> restrictionTracker() {
+                    throw new UnsupportedOperationException("RestrictionTracker parameters are not supported.");
+                }
 
-            @Override
-            public FinishBundleContext finishBundleContext(DoFn<InputT, OutputT> doFn) {
-                throw new IllegalStateException(
-                        "Should not access finishBundleContext() from @"
-                                + DoFn.ProcessElement.class.getSimpleName());
-            }
+                @Override
+                public BoundedWindow window() {
+                    throw new UnsupportedOperationException(
+                            "Access to window of the element not supported in Splittable DoFn");
+                }
 
-            @Override
-            public DoFn<InputT, OutputT>.OnTimerContext onTimerContext(
-                    DoFn<InputT, OutputT> doFn) {
-                throw new UnsupportedOperationException(
-                        "Access to timers not supported in Splittable DoFn");
-            }
+                @Override
+                public StartBundleContext startBundleContext(DoFn<InputT, OutputT> doFn) {
+                    throw new IllegalStateException(
+                            "Should not access startBundleContext() from @"
+                                    + DoFn.ProcessElement.class.getSimpleName());
+                }
 
-            @Override
-            public State state(String stateId) {
-                throw new UnsupportedOperationException(
-                        "Access to state not supported in Splittable DoFn");
-            }
+                @Override
+                public FinishBundleContext finishBundleContext(DoFn<InputT, OutputT> doFn) {
+                    throw new IllegalStateException(
+                            "Should not access finishBundleContext() from @"
+                                    + DoFn.ProcessElement.class.getSimpleName());
+                }
 
-            @Override
-            public Timer timer(String timerId) {
-                throw new UnsupportedOperationException(
-                        "Access to timers not supported in Splittable DoFn");
-            }
-        });
+                @Override
+                public DoFn<InputT, OutputT>.OnTimerContext onTimerContext(
+                        DoFn<InputT, OutputT> doFn) {
+                    throw new UnsupportedOperationException(
+                            "Access to timers not supported in Splittable DoFn");
+                }
+
+                @Override
+                public State state(String stateId) {
+                    throw new UnsupportedOperationException(
+                            "Access to state not supported in Splittable DoFn");
+                }
+
+                @Override
+                public Timer timer(String timerId) {
+                    throw new UnsupportedOperationException(
+                            "Access to timers not supported in Splittable DoFn");
+                }
+            });
+        } catch (UserCodeException uce) {
+            processContext.output(errorOutput, processContext.element());
+        }
     }
-
 }
--- examples/java8/src/main/java/org/talend/beam/examples/handledDoFnExceptionPipeline.java
+++ examples/java8/src/main/java/org/talend/beam/examples/handledDoFnExceptionPipeline.java
@@ -30,6 +30,9 @@ import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TupleTagList;
 
 import java.util.Arrays;
 import java.util.List;
@@ -39,25 +42,25 @@ import static org.apache.beam.sdk.transforms.MapElements.via;
 
 /**
  * An example that counts words in Shakespeare.
- *
+ * <p>
  * <p>This class, {@link handledDoFnExceptionPipeline}, is the first in a series of four successively more
  * detailed 'word count' examples. Here, for simplicity, we don't show any error-checking or
  * argument processing, and focus on construction of the pipeline, which chains together the
  * application of core transforms.
- *
+ * <p>
  * <p>Next, see the {@link WordCount} pipeline, then the {@link DebuggingWordCount}, and finally the
  * {@link WindowedWordCount} pipeline, for more detailed examples that introduce additional
  * concepts.
- *
+ * <p>
  * <p>Concepts:
- *
+ * <p>
  * <pre>
  *   1. Reading data from text files
  *   2. Specifying 'inline' transforms
  *   3. Counting items in a PCollection
  *   4. Writing data to text files
  * </pre>
- *
+ * <p>
  * <p>No arguments are required to run this pipeline. It will be executed with the DirectRunner. You
  * can see the results in the output files in your current working directory, with names like
  * "wordcounts-00001-of-00005. When running on a distributed service, you would use an appropriate
@@ -65,51 +68,59 @@ import static org.apache.beam.sdk.transforms.MapElements.via;
  */
 public class handledDoFnExceptionPipeline {
 
-  public static void main(String[] args) {
-    // Create a PipelineOptions object. This object lets us set various execution
-    // options for our pipeline, such as the runner you wish to use. This example
-    // will run with the DirectRunner by default, based on the class path configured
-    // in its dependencies.
-    PipelineOptions options = PipelineOptionsFactory.create();
+    public static void main(String[] args) {
+        // Create a PipelineOptions object. This object lets us set various execution
+        // options for our pipeline, such as the runner you wish to use. This example
+        // will run with the DirectRunner by default, based on the class path configured
+        // in its dependencies.
+        PipelineOptions options = PipelineOptionsFactory.create();
 
-    // Create the Pipeline object with the options we defined above.
-    Pipeline p = Pipeline.create(options);
+        // Create the Pipeline object with the options we defined above.
+        Pipeline p = Pipeline.create(options);
 
+        final List<String> LINES = Arrays.asList(
+                "To be, or not to be: that is the EXCEPTION question: ",
+                "Whether 'tis nobler in the mind to suffer ",
+                "The slings and arrows of outrageous fortune, ",
+                "Or to take arms against a sea of troubles, ");
 
-      final List<String> LINES = Arrays.asList(
-              "To be, or not to be: that is the EXCEPTION question: ",
-              "Whether 'tis nobler in the mind to suffer ",
-              "The slings and arrows of outrageous fortune, ",
-              "Or to take arms against a sea of troubles, ");
+        ExceptionHandlerDoFn<String, String> exceptionHandlerDoFn = new ExceptionHandlerDoFn<String, String>(new DoFnRaisingException());
 
-      // Create a PCollection from in-memory data
-      p.apply(Create.of(LINES)).setCoder(StringUtf8Coder.of())
+        // Create a PCollection from in-memory data
+        PCollectionTuple results = p.apply("Create", Create.of(LINES)).setCoder(StringUtf8Coder.of())
 
-     // Apply a user defined DoFn
-     .apply("ExtractWords", ParDo.of(new ExceptionHandlerDoFn<String,String>(new DoFnRaisingException())))
+                // Apply a user defined DoFn extracting words and raising exceptions
+                .apply("ExtractWords", ParDo.of(new ExceptionHandlerDoFn<String, String>(new DoFnRaisingException()))
+                        .withOutputTags(exceptionHandlerDoFn.mainOutput, TupleTagList.of(exceptionHandlerDoFn.errorOutput)));
 
-     // Concept #3: Apply the Count transform to our PCollection of individual words. The Count
-     // transform returns a new PCollection of key/value pairs, where each key represents a unique
-     // word in the text. The associated value is the occurrence count for that word.
-     .apply(Count.<String>perElement())
+        // Apply the Count transform to the mainOutput PCollection of individual words. The Count
+        // transform returns a new PCollection of key/value pairs, where each key represents a unique
+        // word in the text. The associated value is the occurrence count for that word.
+        results.get(exceptionHandlerDoFn.mainOutput).setCoder(StringUtf8Coder.of())
 
-     // Apply a MapElements transform that formats our PCollection of word counts into a printable
-     // string, suitable for writing to an output file.
-     .apply("FormatResults", via(new SimpleFunction<KV<String, Long>, String>() {
-                       @Override
-                       public String apply(KV<String, Long> input) {
-                         return input.getKey() + ": " + input.getValue();
-                       }
-                     }))
+                .apply(Count.<String>perElement())
 
-     // Concept #4: Apply a write transform, TextIO.Write, at the end of the pipeline.
-     // TextIO.Write writes the contents of a PCollection (in this case, our PCollection of
-     // formatted strings) to a series of text files.
-     //
-     // By default, it will write to a set of files with names like wordcount-00001-of-00005
-     .apply(TextIO.write().to("wordcounts"));
+                // Apply a MapElements transform that formats our PCollection of word counts into a printable
+                // string, suitable for writing to an output file.
+                .apply("FormatResults", via(new SimpleFunction<KV<String, Long>, String>() {
+                    @Override
+                    public String apply(KV<String, Long> input) {
+                        return input.getKey() + ": " + input.getValue();
+                    }
+                }))
 
-    // Run the pipeline.
-    p.run().waitUntilFinish();
-  }
+                // Apply a write transform, TextIO.Write, at the end of the pipeline.
+                // TextIO.Write writes the contents of a PCollection (in this case, our PCollection of
+                // formatted strings) to a series of text files.
+                //
+                // By default, it will write to a set of files with names like wordcount-00001-of-00005
+                .apply(TextIO.write().to("goodwords"));
+
+        // Output bad words from errorOutput
+        results.get(exceptionHandlerDoFn.errorOutput).setCoder(StringUtf8Coder.of())
+                .apply(TextIO.write().to("badwords"));
+
+        // Run the pipeline.
+        p.run().waitUntilFinish();
+    }
 }
