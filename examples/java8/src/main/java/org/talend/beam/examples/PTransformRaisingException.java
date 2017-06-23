51d8ab165804cd8d9d73ab2859c3bdc57f40adc4
BOURGEOIS Laurent <lbourgeois@talend.com>
2017-06-23 17:33:51 +0200

    Try to catch exception from PTransform

--- examples/java8/src/main/java/org/talend/beam/examples/PTransformRaisingException.java
+++ examples/java8/src/main/java/org/talend/beam/examples/PTransformRaisingException.java
@@ -1,7 +1,48 @@
 package org.talend.beam.examples;
 
+import com.google.api.services.bigquery.model.TableRow;
+import org.apache.beam.examples.WordCount;
+import org.apache.beam.examples.common.ExampleUtils;
+import org.apache.beam.sdk.metrics.Counter;
+import org.apache.beam.sdk.metrics.Metrics;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.util.StringUtils;
+import org.apache.beam.sdk.values.PCollection;
+
 /**
  * Created by lbourgeois on 23/06/17.
+ * Sample PTransform raising an exception when reading EXCEPTION word
  */
-public class PTransformRaisingException {
+public class PTransformRaisingException extends PTransform<PCollection<String>, PCollection<String>>  {
+
+    static class ExtractWordsFn extends DoFn<String, String> {
+        private final Counter emptyLines = Metrics.counter(PTransformRaisingException.ExtractWordsFn.class, "emptyLines");
+
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            if (c.element().trim().isEmpty()) {
+                emptyLines.inc();
+            }
+
+            // Split the line into words.
+            String[] words = c.element().split(ExampleUtils.TOKENIZER_PATTERN);
+
+            // Output each word encountered into the output PCollection.
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    if (word.equals("EXCEPTION")){
+                        throw new IllegalStateException("Illegal state Exception from PTransform");
+                    }
+                    c.output(word);
+                }
+            }
+        }
+    }
+
+    @Override
+    public PCollection<String> expand(PCollection<String> input) {
+        return input.apply(ParDo.of(new PTransformRaisingException.ExtractWordsFn()));
+    }
 }
---
+++ examples/java8/src/main/java/org/talend/beam/examples/handledPTransformExceptionPipeline.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.talend.beam.examples;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.values.PCollection;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Simple pipeline with handled exception from DoFn
+ */
+public class handledPTransformExceptionPipeline {
+
+    public static void main(String[] args) {
+
+        // Pipeline creation
+        PipelineOptions options = PipelineOptionsFactory.create();
+        Pipeline p = Pipeline.create(options);
+
+        // Sample input data
+        final List<String> LINES = Arrays.asList(
+                "To be, or not to be: that is the question: ",
+                "Whether 'tis nobler in the EXCEPTION mind to suffer ",
+                "The slings and arrows of outrageous fortune, ",
+                "Or to take arms against a sea of troubles, ");
+
+        // DoFn which raise exception when reading EXCEPTION word
+        PTransformRaisingException pTransformRaisingException = new PTransformRaisingException();
+
+        // Apply the PTransform on the sample data with 2 outputs : one for the good words and one for the bad words (when exception raised)
+        PCollection results = null;
+
+        try {
+            results = p.apply("Create", Create.of(LINES)).setCoder(StringUtf8Coder.of())
+                    .apply("ExtractWords", pTransformRaisingException);
+        } catch (Exception e) {
+            //TODO
+        }
+
+        results.setCoder(StringUtf8Coder.of())
+                .apply(TextIO.write().to("ptransformout"));
+
+        // Run the pipeline.
+        p.run().waitUntilFinish();
+    }
+}
